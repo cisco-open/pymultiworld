f9d4926bd85a6f1f0095df2bd1326df864a08c01
raresgaia123 <137071040+raresgaia123@users.noreply.github.com>
2024-08-20 19:14:56 +0300

    fix: added mock import (#82)
    added torch to autodoc mock import
    
    Co-authored-by: Rares Gaia <ragaia@cisco.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,7 +51,7 @@ exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
+autodoc_mock_imports = ['torch']
 html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
 napoleon_google_docstring = True
